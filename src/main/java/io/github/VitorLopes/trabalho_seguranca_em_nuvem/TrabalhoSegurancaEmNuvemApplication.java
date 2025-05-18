16ab38eb22c150409ee872bd2a208638589788a7
Vitor Lopes <166652712+VitorLopesCase@users.noreply.github.com>
2025-05-18 13:49:28 -0300

    initial commit

---
+++ src/main/java/io/github/VitorLopes/trabalho_seguranca_em_nuvem/TrabalhoSegurancaEmNuvemApplication.java
@@ -0,0 +1,13 @@
+package io.github.VitorLopes.trabalho_seguranca_em_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoSegurancaEmNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoSegurancaEmNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/VitorLopes/trabalho_seguranca_em_nuvem/TrabalhoSegurancaEmNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.VitorLopes.trabalho_seguranca_em_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoSegurancaEmNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
